16c0c5284691cfc9053830c2318e66538c0337a2
Morgan VanYperen <morgvanny@gmail.com>
2022-07-21 09:54:10 -0700

    commit tree 959930140b6d21d92d7951ac0560589eeb313986

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+  // let p = document.querySelector("p");
+
+  // let p = document.getElementById("text");
+
+  let p = document.querySelector("#text");
+
+  p.addEventListener("click", function () {
+    console.log("clicked");
+  });
+  debugger;
+  p.textContent = "This is really cool!";
+});
